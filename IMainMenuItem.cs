af437f9277369147fffab266012bf049a5eba016
Nick Gelotte <nick.gelotte@rainforgrowth.com>
2022-12-03 23:20:33 -0800

    Initial Checkin

---
+++ IMainMenuItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAP.Types
+{
+	public interface IMainMenuItem : IMainAreaItem
+	{
+		/// <summary>
+		/// Shown in the Menu
+		/// </summary>
+		public string MenuItemName { get; }
+	}
+}
