e186cba94f86ab2fd1534a3f61359bf9e143580c
Nick Gelotte <nick.gelotte@rainforgrowth.com>
2022-12-25 22:17:17 -0800

    IBapProviderChanger now requires GetCurrentBapProvider

--- IBapProviderChanger.cs
+++ IBapProviderChanger.cs
@@ -13,22 +13,29 @@ namespace BAP.Types
         /// Update the Bap Provider
         /// </summary>
         /// <typeparam name="T">The type of IBapProvider that is intended to be updated. Like IButtonProvider or ILayoutProvider</typeparam>
-        /// <param name="t">This must be a type of T or it will return false.  </param>
+        /// <param name="t">This must implement T it will return false.  </param>
         /// <returns>True means it sucesfully updated. False, means it failed to update for any reason.</returns>
         public bool SetNewBapProvider<T>(Type typeOfNewItem) where T : IBapProvider;
         /// <summary>
         /// Update the Bap Provider - this is only for providers types that allow multiple providers
         /// </summary>
         /// <typeparam name="T">The type of IBapProvider that is intended to be add. Like IButtonProvider or ILayoutProvider</typeparam>
-        /// <param name="t">This must be a type of T or it will return false.  </param>
+        /// <param name="t">This must implement T it will return false.  </param>
         /// <returns>True means it sucesfully updated. False, means it failed to update for any reason.</returns>
         public bool AddBapProvider<T>(Type typeOfNewItem) where T : IBapProvider;
         /// <summary>
         /// Update the Bap Provider- this is only for providers types that allow multiple providers
         /// </summary>
         /// <typeparam name="T">The type of IBapProvider that is intended to be updated. Like IButtonProvider or ILayoutProvider</typeparam>
-        /// <param name="t">This must be a type of T or it will return false.  </param>
+        /// <param name="t">This must be a implement T or it will return false.  </param>
         /// <returns>True means it sucesfully updated. False, means it failed to update for any reason.</returns>
         public bool RemoveBapProvider<T>(Type typeOfNewItem) where T : IBapProvider;
+
+        /// <summary>
+        /// Get Bap Provider- this is only for providers types that allow multiple providers
+        /// </summary>
+        /// <typeparam name="T">The Parent type of the BapProvider that you want to retrieve like IButtonProvider or ILayoutProvider/typeparam>
+        /// <returns>True means it sucesfully updated. False, means it failed to update for any reason.</returns>
+        public bool GetCurrentBapProvider<T>() where T : IBapProvider;
     }
 }
