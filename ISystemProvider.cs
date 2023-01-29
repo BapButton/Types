5a766a91a14223952f3f5db6676bd8f80174b65e
Nick Gelotte <nick.gelotte@rainforgrowth.com>
2023-01-29 13:35:17 -0800

    Adding iSystemProvider

---
+++ ISystemProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAP.Types
+{
+    [BapProviderInterface("System Provider", "Make changes to the underlying system - reboot, etc", false)]
+    internal interface ISystemProvider : IBapProvider
+    {
+        public Task<bool> RebootSystem();
+        public Task<bool> RebootWebApp();
+        public Task<bool> RebootWifi();
+        public Task<bool> ShutdownSystem();
+    }
+}
