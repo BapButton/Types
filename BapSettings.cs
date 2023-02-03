a343f90d7c1f463e20d2c940ee7b946fc22af442
Nick Gelotte <nick.gelotte@rainforgrowth.com>
2023-02-02 19:29:30 -0800

    Adding BapSettings

---
+++ BapSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAP.Types
+{
+    public class BapSettings
+    {
+        public string DBConnectionString { get; set; } = "";
+        public string AddonSaveLocation { get; set; } = "";
+    }
+}
