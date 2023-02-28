dac4aae9317cfa4204a26b23f5a6e4da27b0232d
Nick Gelotte <nick.gelotte@rainforgrowth.com>
2023-02-27 21:39:44 -0800

    IGameDataSaver is now like ILogger

--- IGameDataSaver.cs
+++ IGameDataSaver.cs
@@ -7,18 +7,19 @@ using System.Threading.Tasks;
 
 namespace BAP.Types
 {
-	/// <summary>
-	/// Default interface for saving information that a game needs. This is stored in the DB and can be fetched any time the game is active.
-	/// It always saves the information for the currently active game. 
-	/// So if this is used by an auxilary item that kind of acts like a game it won't save and retrieve correctly. 
-	/// </summary>
-	public interface IGameDataSaver
-	{
-		Task<List<(string difficulty, string difficultyDescription)>> GetCurrentScoreBoards();
-		Task<List<Score>> GetScores(string difficulty, int topScoresToTake = 10, bool higherScoreIsBetter = true);
-		Task<Score> AddScore(Score newScore);
-		Task<T?> GetGameStorage<T>();
-		Task<bool> UpdateGameStorage<T>(T itemToSave);
-		Task<List<Score>> GetScoresWithNewScoreIfWarranted(Score newScore, int topScoresToTake = 10, bool higherScoreIsBetter = true);
-	}
+    /// <summary>
+    /// Default interface for saving information that a game needs. This is stored in the DB and can be fetched any time the game is active.
+    /// It always saves the information for the currently active game. 
+    /// So if this is used by an auxilary item that kind of acts like a game it won't save and retrieve correctly. 
+    /// </summary>
+    public interface IGameDataSaver<out TSavedForClassName>
+    {
+        public string UniqueIdUserForSaving { get; }
+        Task<List<(string difficulty, string difficultyDescription)>> GetCurrentScoreBoards();
+        Task<List<Score>> GetScores(string difficulty, int topScoresToTake = 10, bool higherScoreIsBetter = true);
+        Task<Score> AddScore(Score newScore);
+        Task<T?> GetGameStorage<T>();
+        Task<bool> UpdateGameStorage<T>(T itemToSave);
+        Task<List<Score>> GetScoresWithNewScoreIfWarranted(Score newScore, int topScoresToTake = 10, bool higherScoreIsBetter = true);
+    }
 }
